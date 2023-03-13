5d84b1350318b965b7f5d8f14373f26e49f2ab84
hubuke <hubuke@foxmail.com>
2023-03-13 12:15:00 -0700

    finish EE469

--- top.sv
+++ top.sv
@@ -13,7 +13,7 @@ module top(CLK100MHZ, SW, LED);
 
     clock_divider clk_divider (.clock(CLK100MHZ), .reset(SW[15]), .divided_clocks(divided_clocks));
 
-    riscv32_pipeline cpu (.clk(divided_clocks[i] & !result_out), .reset(SW[0]), .result_out(result_out), .reg_10, .mem_800);
+    riscv32_pipeline cpu (.clk(divided_clocks[i] & !(mem_800 == 32'h0000000d)), .reset(SW[0]), .result_out(result_out), .reg_10, .mem_800);
 
     assign LED[0] = result_out;
     assign LED[1] = divided_clocks[i];
